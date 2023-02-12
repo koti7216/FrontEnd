8937c5102d3f9fb4f67bcfd183d0a6f5a08521f5
Koti <koti7216@gmail.com>
2023-02-11 23:09:03 -0500

    About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,6 @@ class About extends Component {
         return (
             <div className='App App-header'>
                 <h1>About</h1>
-                <p>This is user-service full stack development. BackEnd code written in java with Spring-Boot and Front End in react.</p>
             </div>
         );
     }
