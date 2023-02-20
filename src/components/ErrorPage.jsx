76e3c258f4875425732f6e18c8c1a2a2751312a5
Koti <koti7216@gmail.com>
2023-02-19 23:33:54 -0500

    error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom';
 class ErrorPage extends Component {
     render() {
         return (
-            <div>
+            <div className='App error'>
                 <h1>Entered password or user id is wrong</h1>
-                <div>
-                        if you want to create new account <Link to='/signup'>click here</Link>
+                if you want to create new account <Link to='/signup'>click here</Link>
                         <p>if you want to goback to signin <Link to='/'>click here</Link></p> 
+                <div>  
+                        <img src='https://cdncontribute.geeksforgeeks.org/wp-content/uploads/404.png'></img>
+                        
                     </div>
             </div>
         );
